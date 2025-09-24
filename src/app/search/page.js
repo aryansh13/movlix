c3b0d8b9d846dd61a8c4cc5dbe8d75021f7d69b1
aryaas_ <akunto21@gmail.com>
2025-09-24 19:56:05 +0700

    fix production issues

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
+export const dynamic = 'force-dynamic';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import MovieCard from '../components/MovieCard'
